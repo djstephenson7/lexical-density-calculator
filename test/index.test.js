0732f398f79fa564fdf962078274f963915be4a9
djstephenson7 <dan_0502@hotmail.co.uk>
2019-03-16 11:39:18 +0000

    Steup Mocha

---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+var sinon = require('sinon');
+var chai = require('chai');
+var expect = chai.expect;
+
+var mongoose = require('mongoose');
+require('sinon-mongoose');
+
+//Importing our todo model for our unit testing.
+var Words = require('../app/models/WordSchema');
